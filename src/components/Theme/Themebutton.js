622269dd3abc11cda4d7c1346382152e7481fbdf
Flexxelexx <104971956+Flexxelexx@users.noreply.github.com>
2023-02-08 11:46:08 +0100

    lele

---
+++ src/components/Theme/Themebutton.js
@@ -0,0 +1,29 @@
+import {BsFillSunFill} from "react-icons/bs";
+import {MdDarkMode} from "react-icons/md";
+import React from "react";
+import useLocalStorage from "use-local-storage";
+
+function Colortheme() {
+
+    const defaultDark = window.matchMedia('(prefers-color-scheme: donker)').matches;
+    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'donker' : 'licht');
+
+    const switchTheme = () => {
+        const newTheme = theme === 'donker' ? 'licht' : 'donker';
+        setTheme(newTheme)
+    }
+
+    return (
+
+        <div>
+            <button
+                className='themebtn'
+                onClick={switchTheme}
+            > {theme === 'donker' ? <BsFillSunFill/> : <MdDarkMode/>}
+            </button>
+        </div>
+
+    )
+};
+
+export default Colortheme()
